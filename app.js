91bb56251e65a88473f060b2613a995d2fe7e96f
Dani Martinez <dan256_6@hotmail.com>
2016-11-08 23:28:21 +0100

    Added ajax remove

--- app.js
+++ app.js
@@ -15,24 +15,43 @@ app.use(express.static('public'));
 var count= 0;
 let tasks = []
 
+// Client
+app.get('/tasks', (req,res) => {
+  let enabledTasks = tasks.filter((task) => {
+    return task.status === true;
+  });
+
+  res.json(enabledTasks);
+});
+
 
 app.get('/data', (req,res) => {
-
 	oData = tasks;
 	console.log(tasks);
 	res.json( oData );
+});
 
-})
-
-app.post('/data', (req,res) => {
+app.post('/', (req,res) => {
+  console.log('asdasdasd');
 	var taskName = req.body.description;
 	const newTask = {
       "id": count++,
       "description": taskName,
       "done": "false"
-  }
-	tasks.push(newTask);
-	res.sendStatus(200)
-})
+  };
 
-app.listen(3000, () =>  console.log("Listening on port 3000...."))
+	tasks.push(newTask);
+	res.sendFile(`${__dirname}/public/index.html`);
+});
+
+//
+app.delete('/data/:id', (req, res) => {
+  tasks = tasks.filter((task) => {
+    return task.id !== parseInt(req.params.id);
+  });
+
+  console.log(tasks);
+  res.json(tasks);
+});
+
+app.listen(3000, () =>  console.log("Listening on port 3000...."));
--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,58 @@
-$("#task_form").on('submit', (event) => {
+$( document ).ready(() => {
 
-	event.preventDefault();
-
-
-	$.ajax({
-		url: `/data`
-	})
-	.done(function( aTasks ) {
-		
-		var nameTasks = aTasks.map( task => `<li id="${task.id}"> ${task.description} => State: ${task.done} 
-			</li> <button type="button" class="deleteTask btn btn-primary">Delete</button>` )
-		$("#tasks").html( nameTasks.join('') )
-
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
+	// Get all tasks
+	$("#getTasksBtn").click(() => {
+		getTasks();
 	});
 
+	// // Add new task
+	// $("#addTaskBtn").click(() => {
+	// 	addTask();
+	// });
+
+	//
+	$('body').on('click', '.deleteTask', (e) => {
+		let $li = $(e.target).parent();
+		let taskId = $li.attr('id');
+
+		removeTaskById(taskId);
+		getTasks();
+	});
+
+	//
+	function getTasks() {
+		$.ajax({
+			url: `/data`
+		})
+		.done(function( aTasks ) {
+			
+			var nameTasks = aTasks.map( task => `
+				<li id="${task.id}"> ${task.description} => State: ${task.done} 
+					<button type="button" class="deleteTask btn btn-primary">Delete</button>
+				</li>`);
+
+			$("#tasks").html( nameTasks.join('') );
+
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	}
+
+	// 
+	function removeTaskById(taskId) {
+		$.ajax({
+			url: `/data/${taskId}`,
+			method: 'DELETE'
+
+		}).success((response) => {
+			console.log('success', response);
+
+		}).error((error) => {
+			console.log('error', error);
+		});
+	}
 });
-
-
-
